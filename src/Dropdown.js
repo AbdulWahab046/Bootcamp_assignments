46601cc2489b07189c8869aaa9eace85828b299b
AliMuhtashim <ali184419@gmail.com>
2022-07-30 03:40:36 -0700

    aj ka kam

---
+++ src/Dropdown.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+
+export default function Dropdown({setFilterData,data}) {
+   const handleChange=(event)=>{
+        console.log(event.target.value);
+    const filterPhoto = data.photos.filter((value)=>value.heading === event.target.value);
+   setFilterData(filterPhoto);
+    };
+ 
+
+  return (
+    <Box sx={{ minWidth: 150 }}>
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Card</InputLabel>
+    <Select
+        onChange={handleChange}
+        label="Photo">
+        {data.photos.map((value)=>{
+              return(
+                <MenuItem value={value.heading} key={value.id}>
+                    {value.heading}
+                </MenuItem>
+            )
+
+        })
+        }
+              
+      </Select>
+    </FormControl>
+  </Box>
+  )
+}
